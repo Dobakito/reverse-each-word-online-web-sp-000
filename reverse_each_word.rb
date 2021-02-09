b22da8c0f2af3a993024012f3eef05b9c39f30cd
Michael Dobak <mdobak44@icloud.com>
2021-02-09 19:51:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each do |x|
-    x.reverse
+  reversed_array = new_array.each do |words|
+    words.reverse
   return reversed_array.join(" ")
 end
